11485ad5465386b0266d44efe3b8ef92ea29c436
Romina Calleja <romycalleja.12.08@gmail.com>
2022-10-26 20:47:29 -0300

    terminos

--- registro2/js/validacion.js
+++ registro2/js/validacion.js
@@ -1,44 +1,39 @@
-
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-  'use strict'
+  "use strict";
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  let forms = document.querySelectorAll('.needs-validation')
+  let forms = document.querySelectorAll(".needs-validation");
   // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
         if (!form.checkValidity() || validarcontra()) {
-         
-            event.preventDefault()
-            event.stopPropagation()
-            checked()
-          }
-         
-      form.classList.add('was-validated')
-    
-      }, false)
-    })
-})()
+          event.preventDefault();
+          event.stopPropagation();
+          checked();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
 
 const contra1 = document.getElementById("validationCustom05");
 const contra2 = document.getElementById("validationCustom03");
 
-
-
-contra2.addEventListener("input",validarcontra);
+contra2.addEventListener("input", validarcontra);
 
 function validarcontra() {
- 
   if (contra1.value === contra2.value) {
     contra2.classList.add("is-valid");
     console.log("hola");
     contra2.classList.remove("is-invalid");
     contra2.setCustomValidity("");
     return false;
-  }
-  else {
+  } else {
     contra2.classList.add("is-invalid");
     console.log("chau");
     contra2.classList.remove("is-valid");
@@ -47,38 +42,25 @@ function validarcontra() {
   }
 }
 
+document.getElementById("invalidCheck").addEventListener("click",checked);
 
-function elemento() {
+function checked() {
+  elemento = document.getElementById("invalidCheck");
 
-  elemento = document.getElementById("terminos");
-  if (!elemento.checked) {
-   
-    let error=""
-     error+=`
-     <p> Aceptar terminos</p>
-     `
-
-     document.getElementById("errorterminos").innerHTML=error 
-     return false;
-  }
-
-}
-document.getElementById("invalidCheck").addEventListener("input",checked);
-
-function checked(){
-  console.log(document.getElementById("invalidCheck").checked)
+  console.log(elemento.checked);
 
   if (!elemento.checked) {
-   
-    let error=""
-     error+=`
-     <p> Aceptar terminos</p>
-     `
+    let error = "";
+    error += `
+     <p class="rojo"> Aceptar terminos</p>
+     `;
 
-     document.getElementById("errorterminos").innerHTML=error 
-  }
-  else{
-    let error=""
-    document.getElementById("errorterminos").innerHTML=error 
+    document.getElementById("errorterminos").innerHTML = error;
+  } else {
+    let error = "";
+    error += `
+    <p></p>
+     `;
+    document.getElementById("errorterminos").innerHTML = error;
   }
 }
