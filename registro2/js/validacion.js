3ece6a1267dc36303317172a99f95e67a2fa1faf
pablo <pablodos444@gmail.com>
2022-10-26 19:30:23 -0300

    validacion

--- registro2/js/validacion.js
+++ registro2/js/validacion.js
@@ -8,47 +8,52 @@
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
-       
-        if (!form.checkValidity()) {
-          if(validarcontra()){
-          event.preventDefault()
-          event.stopPropagation()
+
+        if (!form.checkValidity() || validarcontra()  ) {
+         
+            event.preventDefault()
+            event.stopPropagation()
+
           }
+        
           
-        } 
-
-
-        form.classList.add('was-validated')
+     
+      form.classList.add('was-validated')
+    
       }, false)
     })
 })()
 
 const contra1 = document.getElementById("validationCustom05");
 const contra2 = document.getElementById("validationCustom03");
-const boton = document.getElementById("button");
-
-
 
 
 function validarcontra() {
 
-if( contra1.value === contra2.value) {
+  if (contra1.value === contra2.value) {
+    contra2.classList.add("is-valid");
+    console.log("hola");
+    contra2.classList.remove("is-invalid");
+    contra2.setCustomValidity("");
+    return false;
+  }
 
-  contra2.classList.add("is-valid");
 
-  console.log("hola");   
-  contra2.classList.remove("is-invalid")
-    return false;  
-   
+  else {
+    contra2.classList.add("is-invalid");
+    console.log("chau");
+    contra2.classList.remove("is-valid");
+    contra2.setCustomValidity("error");
+    return true;
+  }
 }
 
-else{
-  contra2.classList.add("is-invalid");   
-  console.log("chau");
-  contra2.classList.remove("is-valid")
-  return true;  
+
+function elemento() {
+
+  elemento = document.getElementById("terminos");
+  if (!elemento.checked) {
+    return false;
+  }
+
 }
-
-
-}
-
